2a79cba933ab8f347b26b26cdb36fe9d8610ce6c
Mikhail Malyshev <malyshev090994@gmail.com>
2020-04-01 13:32:04 +0300

    Initial Commit

---
+++ LanguageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LanguageApp
+//
+//  Created by Михаил Малышев on 01/04/2020.
+//  Copyright © 2020 Mikhail Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
